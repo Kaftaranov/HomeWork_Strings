7a50ee64db480d90d41f0397652276ced67a1401
Timur Kaftaranov <kaftaranov@rambler.ru>
2022-05-08 21:08:40 +0600

    First commit of Home Work

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args)
+    {
+//Задача №1
+        String name = "Ivan";
+        String middleName = "Ivanovich";
+        String surname = "Ivanov";
+        String fullName = surname + " " + name + " " + middleName;
+        System.out.println("Name of employee is " + fullName);
+        System.out.println("-------------------------------------------------------");
+//Задача №2
+        System.out.println("Name of employee for the report: " + fullName.toUpperCase());
+        System.out.println("-------------------------------------------------------");
+//Задача №3
+        name = "Semyon";
+        middleName = "Semyonovich";
+        surname = "Ivanov";
+        fullName = surname + " " + name + " " + middleName;
+        for (int i = 0; i < fullName.length(); i++) {fullName = fullName.replace("yo","e");}
+        System.out.println("Full name of employee is " + fullName);
+//Задача №4
+
+    }
+}
